d20aaf605c3f62827c7add77158654daae1a5353
Dima <dima23790@gmail.com>
2020-04-15 15:53:19 +0300

    Added quick sort

--- sortAlgorithms.js
+++ sortAlgorithms.js
@@ -81,4 +81,33 @@ const mergeSort = (arr) => {
   return merge(left, right);
 };
 
-console.log(mergeSort([32, 1645, 21, 64, 3, 123, 8, 43, 11, 663]));
+// console.log(mergeSort([32, 1645, 21, 64, 3, 123, 8, 43, 11, 663]));
+
+//================= Quick Sort ==================
+const pivot = (arr, start = 0, end = arr.length - 1) => {
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
+    }
+  }
+  [arr[start], arr[swapIdx]] = [arr[swapIdx], arr[start]];
+
+  return swapIdx;
+};
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    //left side
+    quickSort(arr, left, pivotIndex - 1);
+    //right side
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+};
+
+// console.log(quickSort([4, 8, 2, -3, 100, 632, 1, 5, 7, 6, 3]));
